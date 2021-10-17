9de2bb09d81450bc405ddac2a91b43bc116158da
bujowskis <s.bujowski@gmail.com>
2021-10-17 17:49:29 +0200

    first-order approximation, average word size in this approx

--- main.py
+++ main.py
@@ -1,82 +1,85 @@
 import random
+from math import ceil
 
 
 # 26 lower case letters of English alphabet, digits, space (37 characters total)
 
-# Function outputting zeroth-order approximation using provided set of characters, of some given length (300 by default)
+# Outputs zeroth-order approximation using provided set of characters, of some given length (300 by default)
 def zeroth(chars=None, length=300):
     if chars is None:
         chars = ['a', 'b', 'c', 'd']
     chars_len = len(chars)
 
-    fout = open('zeroth.txt', 'w')
-    for i in range(1, length + 1):
-        fout.write(chars[random.randrange(chars_len)])
+    with open('zeroth.txt', 'w') as fout:
+        for i in range(1, length + 1):
+            fout.write(chars[random.randrange(chars_len)])
 
 
+# Removes digits from given dictionary
 def remove_digits(dictionary):
     for key in [chr(char) for char in range(ord('0'), ord('9') + 1)]:
         if key in dictionary.keys():
             dictionary.pop(key)
 
 
-# Function outputting first-order approximation of some length, given a dictionary
-# def first_order(dict, length=500):
+# Outputs first-order approximation of some length, given frequency dictionary
+def first_order(freq, file_name, length=500):
+    if length < 0:
+        print("length cannot be lower than 0")
+        return
+
+    total = sum(freq.values())
+    for key in freq.keys():
+        freq[key] = freq[key] / total
+
+    # generate approximation
+    with open('first-order-{}'.format(file_name), 'w') as fout:
+        i = 0
+        while i < length:
+            fout.write("{}".format(random.choices(list(freq.keys()), list(freq.values()))[0]))
+            i += 1
+
+    # calculate average word length
+    with open('first-order-{}'.format(file_name), 'r') as file:
+        contents = file.readlines()
+        contents = contents[0].split()
+        word_lenghts = 0
+        for word in contents:
+            word_lenghts += len(word)
+        print("\taverage length of words: {}\n".format(ceil(word_lenghts / len(contents))))
+
+
+# Main function, carrying out all the approximations based on given norm sample
+def run_approx(file_name):
+    print("\n\ncurrent file: {}\n".format(file_name))
+    freq = {}
+    with open(file_name, 'r') as file:
+        contents = file.readlines()
+        for word in contents:
+            for char in word:
+                if char in freq:
+                    freq[char] += 1
+                else:
+                    freq[char] = 1
+
+        print("\tfrequencies:\n{}\n".format(
+            {key: value for key, value in sorted(freq.items(), key=lambda item: item[1], reverse=True)}))
+        remove_digits(freq)
+        print("\tfrequencies(no digits):\n{}\n".format(
+            {key: value for key, value in sorted(freq.items(), key=lambda item: item[1], reverse=True)}))
+        total = sum(freq.values())
+        print("\tno. of characters(no digits): {}\n".format(total))
+
+        first_order(freq, file_name)
 
 
 # MAIN PROGRAM
-# pool of characters used for
+# zeroth-order approximation
 chars = [chr(letter) for letter in range(ord('a'), ord('z') + 2)]
 chars[26] = ' '
-zeroth(chars, 1000)  # zeroth-order approximation
+zeroth(chars, 1000)
 
-freq = {}
-print("\n\nfirst file - hamlet\n")
-with open('norm_hamlet.txt', 'r') as file:
-    contents = file.readlines()
-    for word in contents:
-        for char in word:
-            if char in range(ord('0'), ord('9') + 1):
-                continue
-            if char in freq:
-                freq[char] += 1
-            else:
-                freq[char] = 1
-    print("frequencies in hamlet:\n{}\n".format(
-        {key: value for key, value in sorted(freq.items(), key=lambda item: item[1], reverse=True)}))
-
-    remove_digits(freq)
-    total = sum(freq.values())
-
-print("\n\nsecond file - romeo and juliet\n")
-with open('norm_romeo_and_juliet.txt', 'r') as file:
-    contents = file.readlines()
-    for word in contents:
-        for char in word:
-            if char in range(ord('0'), ord('9') + 1):
-                continue
-            if char in freq:
-                freq[char] += 1
-            else:
-                freq[char] = 1
-    print("frequencies in romeo and juliet:\n{}\n".format(
-        {key: value for key, value in sorted(freq.items(), key=lambda item: item[1], reverse=True)}))
-
-    remove_digits(freq)
-    total = sum(freq.values())
-
-print("\n\nthird file - wiki sample\n")
-with open('norm_wiki_sample.txt', 'r') as file:
-    contents = file.readlines()
-    for word in contents:
-        for char in word:
-            if char in freq:
-                freq[char] += 1
-            else:
-                freq[char] = 1
-    print("frequencies in wiki:\n{}\n".format(
-        {key: value for key, value in sorted(freq.items(), key=lambda item: item[1], reverse=True)}))
-
-    remove_digits(freq)
-    total = sum(freq.values())
-
+# all other approximations
+run_approx("norm_hamlet.txt")
+run_approx("norm_romeo_and_juliet.txt")
+run_approx("norm_wiki_sample.txt")
